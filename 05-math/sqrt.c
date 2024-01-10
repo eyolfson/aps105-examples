f90efd8fb144c3bdceca389ab5ea1b5eb6275e3b
Jonathan Eyolfson <jon@eyolfson.com>
2024-01-10 01:25:23 +0000

    Added Lecture 5 example.

---
+++ 05-math/sqrt.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    double y = 4.0;
+    double y_sqrt = sqrt(y);
+    printf("sqrt(%.1lf) = %.1lf\n", y, y_sqrt);
+    printf("%.0lf\n", 3.5);
+    return EXIT_SUCCESS;
+}
