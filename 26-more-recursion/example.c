c527ab69272d7f49d199c28e23be5fdaed503a10
Jonathan Eyolfson <jon@eyolfson.com>
2024-03-13 15:04:55 +0000

    Restore example.

--- 26-more-recursion/example.c
+++ 26-more-recursion/example.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ARRAY_LENGTH(array) (sizeof((array))/sizeof((array)[0]))
-
-int sum(int *array, int arrayLength) {
-    if (arrayLength == 0) {
-        return 0;
-    }
-    else {
-        return array[0] + sum(array + 1, arrayLength - 1);
-    }
-}
-
 int main(void) {
-    int array[] = {1, 2, 3, 4, 5};
-    int arrayLength = ARRAY_LENGTH(array);
-    printf("%d\n", sum(array, arrayLength));
     return EXIT_SUCCESS;
 }
