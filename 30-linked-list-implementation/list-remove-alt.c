b77d358fb99ec1792b9e22793f2617c601241744
Jonathan Eyolfson <jon@eyolfson.com>
2024-03-27 00:29:18 +0000

    Alternative.

---
+++ 30-linked-list-implementation/list-remove-alt.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node node_t;
+
+typedef struct node {
+    int val;
+    node_t *next;
+} node_t;
+
+typedef struct linked_list {
+    node_t *head;
+} linked_list_t;
+
+linked_list_t *createLinkedList() {
+    linked_list_t *linked_list = malloc(sizeof(linked_list_t));
+    if (linked_list == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    linked_list->head = NULL;
+    return linked_list;
+}
+
+void freeLinkedList(linked_list_t *linked_list) {
+    node_t *current = linked_list->head;
+    free(linked_list);
+    while (current != NULL) {
+        node_t *node = current;
+        current = current->next;
+        free(node);
+    }
+}
+
+node_t *createNode(int val) {
+    node_t *node = malloc(sizeof(node_t));
+    if (node == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    node->val = val;
+    node->next = NULL;
+    return node;
+}
+
+void printLinkedList(linked_list_t *linked_list) {
+    node_t *current = linked_list->head;
+    printf("list:");
+    while (current != NULL) {
+        printf(" %d", current->val);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+node_t *insertFront(linked_list_t *linked_list, int val) {
+    node_t *node = createNode(val);
+    node->next = linked_list->head;
+    linked_list->head = node;
+    return node;
+}
+
+void removeNode(linked_list_t *linked_list, node_t *node) {
+    node_t **p = &linked_list->head;
+    while (*p != node && *p != NULL) {
+        p = &(*p)->next;
+    }
+    if (*p == NULL) {
+        return;
+    }
+    *p = node->next;
+}
+
+int main(void) {
+    linked_list_t *linked_list = createLinkedList();
+    node_t *n4 = insertFront(linked_list, 4);
+    insertFront(linked_list, 3);
+    insertFront(linked_list, 2);
+    insertFront(linked_list, 1);
+    removeNode(linked_list, n4);
+    printLinkedList(linked_list);
+    freeLinkedList(linked_list);
+    return EXIT_SUCCESS;
+}
