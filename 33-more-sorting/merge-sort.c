d6249e83f5105aa02ce7f65bc94aa6a674dfb8cf
Jonathan Eyolfson <jon@eyolfson.com>
2024-04-03 22:23:03 +0000

    Quick sort.

---
+++ 33-more-sorting/merge-sort.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARRAY_LENGTH(array) (sizeof((array))/sizeof((array)[0]))
+
+void printArray(int array[], int arrayLength) {
+    printf("array:");
+    for (int i = 0; i < arrayLength; ++i) {
+        printf(" %d", array[i]);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    return EXIT_SUCCESS;
+}
---
+++ 33-more-sorting/quick-sort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARRAY_LENGTH(array) (sizeof((array))/sizeof((array)[0]))
+
+void printArray(int array[], int arrayLength) {
+    printf("array:");
+    for (int i = 0; i < arrayLength; ++i) {
+        printf(" %d", array[i]);
+    }
+    printf("\n");
+}
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int partition(int array[], int low, int high) {
+    int pivot = array[high];
+    int i = low - 1; /* Temporary pivot index */
+    for(int j = low; j <= high; j++) {
+        if(array[j] < pivot) {
+            /* Move the pivot one element forward */
+            ++i;
+            swap(&array[i], &array[j]);
+        }
+    }
+    ++i;
+    swap(&array[i], &array[high]);
+    return i;
+}
+
+void quickSortHelper(int array[], int low, int high) {
+    if (low >= high) {
+        return;
+    }
+    int pivot = partition(array, low, high);
+    quickSortHelper(array, low, pivot - 1);
+    quickSortHelper(array, pivot + 1, high);
+}
+
+void quickSort(int array[], int arrayLength) {
+    quickSortHelper(array, 0, arrayLength - 1);
+}
+
+int main(void) {
+    int array[] = {10, 14, 8, 13, 20, 3, 6, 9, 4};
+    int arrayLength = ARRAY_LENGTH(array);
+    quickSort(array, arrayLength);
+    printArray(array, arrayLength);
+    return EXIT_SUCCESS;
+}
